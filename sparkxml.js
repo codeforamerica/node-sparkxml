a50e2ebb31bd1728e845521c6f4905e343c613b4
Rob Brackett <rob@robbrackett.com>
2012-09-05 11:42:49 -0700

    Slight refactor and .gitignore

--- sparkxml.js
+++ sparkxml.js
@@ -11,27 +11,22 @@ exports.parseXml = function(xmlString, parseTypes) {
 	return convertElementToObject(document.root(), parseTypes);
 };
 
-var convertElementToObject = function(xmlElement, convertTypes) {
-	var convertTypes = convertTypes === false ? false : true;
+/**
+ * Converts an element (and its children) into an object by using the Spark convention.
+ * This is the main body of all this library.
+ * @param {XMLElement} The XML element to convert
+ * @param {Boolean} [parseTypes=true] Whether to automatically parse data types (e.g. <value>5.1</value> is parsed as a number instead of a string.)
+ *
+ * @private
+ */ 
+var convertElementToObject = function(xmlElement, parseTypes) {
+	var parseTypes = parseTypes === false ? false : true;
 	var childElements = xmlElement.find("./*");
 
 	// if there are no children, parse the content
 	if (childElements.length === 0) {
-		var value =  xmlElement.text();
-		if (convertTypes) {
-			var numberValue = Number(value);
-			var lowerValue = value.toLowerCase();
-			if (!isNaN(numberValue)) {
-				value = numberValue;
-			}
-			else if (lowerValue === "true") {
-				value = true;
-			}
-			else if (lowerValue === "false") {
-				value = false;
-			}
-		}
-		return value;
+		var value = xmlElement.text();
+		return parseTypes ? parseValue(value) : value;
 	}
 	// otherwise create an object/array from the child elements
 	else {
@@ -42,19 +37,17 @@ var convertElementToObject = function(xmlElement, convertTypes) {
 		childElements.forEach(function(childElement) {
 			var childName = childElement.name();
 
-			if (!elementIsList) {
-				// if we're trying to overwrite the same name, we're actually a list
-				if (resultObject.hasOwnProperty(childName)) {
-					elementIsList = true;
-					var newResultObject = [];
-					for (var key in resultObject) {
-						newResultObject.push(resultObject[key]);
-					}
-					resultObject = newResultObject;
+			// if we're trying to overwrite the same name, switch to using an array
+			if (!elementIsList && resultObject.hasOwnProperty(childName)) {
+				elementIsList = true;
+				var newResultObject = [];
+				for (var key in resultObject) {
+					newResultObject.push(resultObject[key]);
 				}
+				resultObject = newResultObject;
 			}
 
-			var childObject = convertElementToObject(childElement, convertTypes);
+			var childObject = convertElementToObject(childElement, parseTypes);
 			if (elementIsList) {
 				resultObject.push(childObject);
 			}
@@ -62,6 +55,32 @@ var convertElementToObject = function(xmlElement, convertTypes) {
 				resultObject[childName] = childObject;
 			}
 		});
+
 		return resultObject;
 	}
+};
+
+/**
+ * Attempts to parse a string as some other type.
+ * Right now, this parses numbers and booleans (case insensitive).
+ * @param {String} value The text to parse
+ * @type {String|Boolean|Number}
+ *
+ * @private
+ */
+var parseValue = function(value) {
+	var numberValue = Number(value);
+	if (!isNaN(numberValue)) {
+		return numberValue;
+	}
+
+	var lowerValue = value.toLowerCase();
+	if (lowerValue === "true") {
+		return true;
+	}
+	else if (lowerValue === "false") {
+		return false;
+	}
+
+	return value;
 };
